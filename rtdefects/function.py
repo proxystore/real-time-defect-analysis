81e879bd6ccfd1a8688f890f3977c7f6a3e640d6
Logan Ward <ward.logan.t@gmail.com>
2021-06-21 12:23:10 -0400

    Add analysis function

--- rtdefects/function.py
+++ rtdefects/function.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from hashlib import md5
 import logging
 
+from skimage import data, filters, measure, morphology
 import tensorflow as tf
 import numpy as np
 
@@ -47,3 +48,32 @@ def perform_segmentation(image_data: np.ndarray) -> np.ndarray:
 
     # Perform the segmentation
     return _model.predict(image_data)
+
+
+def analyze_defects(mask: np.ndarray, min_size: int = 50) -> dict:
+    """Analyze the voids in a masked image
+
+    Args:
+        mask: Masks for a defect image
+        min_size: Minimum size of defects
+    Returns:
+        List of the computed properties
+    """
+
+    # Clean up the mask
+    mask = morphology.remove_small_objects(mask, min_size=min_size)
+    mask = morphology.remove_small_holes(mask, min_size)
+    mask = morphology.binary_erosion(mask, morphology.square(1))
+    output = {'void_frac': mask.sum() / (mask.shape[0] * mask.shape[1])}
+
+    # Assign labels to the labeled regions
+    labels = measure.label(mask)
+    output['void_count'] = labels.max()
+
+    # Compute region properties
+    props = measure.regionprops(labels, mask)
+    radii = [p['equivalent_diameter'] for p in props]
+    output['radii'] = radii
+    output['radii_average'] = np.average(radii)
+
+    return output
--- tests/test_function.py
+++ tests/test_function.py
@@ -3,9 +3,10 @@ from pathlib import Path
 
 import numpy as np
 from pytest import fixture
+from skimage import io
 import cv2
 
-from rtdefects.function import perform_segmentation
+from rtdefects.function import perform_segmentation, analyze_defects
 
 
 @fixture()
@@ -14,7 +15,19 @@ def image() -> np.ndarray:
     return np.array(img[None, :, :, :], np.float32) / 255
 
 
+@fixture()
+def mask() -> np.ndarray:
+    img = io.imread(str(Path(__file__).parent.joinpath('test-image-mask.tif')))
+    return np.array(img)
+
+
 def test_run(image):
     output = perform_segmentation(image)
     assert output.shape == (1, 1024, 1024, 1)
     cv2.imwrite('test-image-mask.tif', output[0])
+
+
+def test_analyze(mask):
+    mask = mask > 0.99
+    output = analyze_defects(mask)
+    assert output['void_frac'] > 0
